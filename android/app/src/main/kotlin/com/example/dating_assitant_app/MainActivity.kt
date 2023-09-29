e7397d61828d1f9818ec96aaf2c6d84a448adc62
oke kolade <koladeoke3@gmail.com>
2023-09-29 13:54:50 +0100

    initail commit

---
+++ android/app/src/main/kotlin/com/example/dating_assitant_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dating_assitant_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ProjectColors {
+  static const Color pink = Color(0xffFF6B6B);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xff000000);
+  static const Color grey = Color(0xffF5F5F5);
+  static const Color blue = Color(0xff4285F4);
+}
