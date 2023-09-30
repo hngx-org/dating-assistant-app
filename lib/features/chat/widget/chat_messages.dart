5732b18814ba8d6ac51ba6e25bb4788234b4dbd7
oke kolade <koladeoke3@gmail.com>
2023-09-30 23:44:55 +0100

    updated chat screen

--- lib/features/chat/widget/chat_messages.dart
+++ lib/features/chat/widget/chat_messages.dart
@@ -1,18 +1,18 @@
-import 'package:dating_assitant_app/commons/widgets/text_widgets.dart';
+// import 'package:dating_assitant_app/commons/widgets/text_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:dating_assitant_app/commons/utils/colors.dart';
+import 'package:dating_assitant_app/features/chat/widget/chat_bubble.dart';
+// import 'package:dating_assitant_app/commons/utils/colors.dart';
 
+class ChatMessage extends StatelessWidget {
+  const ChatMessage({super.key, required this.sender, required this.text});
 
-class ChatMessages extends StatelessWidget {
-  const ChatMessages({super.key});
+  final String text;
+  final String sender;
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: TextSize18(
-        text: 'welcome',
-        color: ProjectColors.black,
-      )
+    return const Row(
+      children: [],
     );
   }
 }
--- lib/features/chat/widget/new_messages.dart
+++ lib/features/chat/widget/new_messages.dart
@@ -1,75 +1,22 @@
-import 'package:flutter/material.dart';
-import 'package:dating_assitant_app/commons/utils/colors.dart';
+class Message {
+  String text;
+  DateTime date;
+  bool isSentByMe;
 
-class NewMessages extends StatefulWidget {
-  const NewMessages({super.key});
-
-  @override
-  State<NewMessages> createState() => _NewMessagesState();
+  Message({required this.text, required this.date, required this.isSentByMe});
 }
 
-class _NewMessagesState extends State<NewMessages> {
-  final _messageController = TextEditingController();
-
-  @override
-  void dispose() {
-    _messageController.dispose();
-    super.dispose();
-  }
-
-  void _submitMessage() {
-    final enteredMessage = _messageController.text;
-
-    if (enteredMessage.trim().isEmpty) {
-      return;
-    }
-
-    // send to backend
-
-    _messageController.clear();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 15, right: 1, bottom: 14),
-      child: Row(children: [
-        Expanded(
-          child: TextField(
-            controller: _messageController,
-            textCapitalization: TextCapitalization.sentences,
-            autocorrect: true,
-            enableSuggestions: true,
-            decoration: InputDecoration(
-              hintText: 'Start a new message',
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(50),
-                borderSide:
-                    const BorderSide(width: 1, color: ProjectColors.pink),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(50),
-                borderSide:
-                    const BorderSide(width: 1, color: ProjectColors.pink),
-              ),
-            ),
-          ),
-        ),
-        ElevatedButton(
-          onPressed: _submitMessage,
-          style: ElevatedButton.styleFrom(
-            backgroundColor: ProjectColors.pink,
-            shape: const CircleBorder(), //<-- SEE HERE
-            padding: const EdgeInsets.all(20),
-          ),
-          child: const Icon(
-            //<-- SEE HERE
-            Icons.send,
-            color: ProjectColors.white,
-            size: 20,
-          ),
-        ),
-      ]),
-    );
-  }
-}
+List<Message> messages = [
+  Message(
+      text: 'Hello',
+      date: DateTime.now().subtract(
+        const Duration(minutes: 1),
+      ),
+      isSentByMe: true),
+  Message(
+      text: 'Hello Sender',
+      date: DateTime.now().subtract(
+        const Duration(minutes: 1),
+      ),
+      isSentByMe: false),
+].reversed.toList();
