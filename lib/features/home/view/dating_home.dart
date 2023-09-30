55bbc584654820288c010413cff4fb38687f9ab1
Chibueze Edeh <choboy.edeh17@gmail.com>
2023-09-30 22:41:20 +0100

    fixed the homescreen

--- lib/features/home/view/dating_home.dart
+++ lib/features/home/view/dating_home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:dating_assitant_app/commons/utils/colors.dart';
 import 'package:dating_assitant_app/commons/widgets/text_widgets.dart';
 import 'package:dating_assitant_app/commons/widgets/custom_container.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,22 +16,33 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-          backgroundColor: ProjectColors.white,
-          title: ListTile(
-            leading: Image.asset(
-              'assets/images/logo.png',
-              width: 50,
-            ),
-            title:  const TextSize18(text: 'Ai Assistant', color: ProjectColors.black),
-          )),
-          backgroundColor: ProjectColors.white,
+      backgroundColor: ProjectColors.white,
       body: Padding(
         padding: const EdgeInsets.only(top: 16, left: 20, right: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            SizedBox(
+              height: 32.h,
+            ),
+            Row(children: [
+              SizedBox(
+                width: 8.w,
+              ),
+              Image.asset('assets/images/logo.png', width: 50),
+              SizedBox(
+                width: 12.w,
+              ),
+              TextSize18(
+                text: 'Ai Assistant',
+                color: ProjectColors.black,
+                fontWeight: FontWeight.normal,
+                fontSize: 18.r,
+              )
+            ]),
+            SizedBox(
+              height: 40.h,
+            ),
             const Textsize32(
               text: 'Welcome to your Ai \ndating assistant',
               color: ProjectColors.pink,
